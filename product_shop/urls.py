06d31f163b86ba8e34cfc123b13465d01f18b284
DodogeldievAmir1 <dodogeldiev1996@mail.ru>
2026-06-01 17:07:10 +0600

    Добавил логику добавления продукта, скорректировал base html и index html, добавил в views логику добавления категорий, добавил пустой html добавления категорий

--- product_shop/urls.py
+++ product_shop/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home_page
+from .views import home_page, add_new_product, add_new_category
 
 
 urlpatterns = [
     path('', home_page, name='home_page'),
-
+    path('add/', add_new_product, name='add'),
+    path('add/category/', add_new_category, name='add_category'),
 ]
--- product_shop/views.py
+++ product_shop/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
+from product_shop.models import Product, Category
+
+
 
 def home_page(request):
-    context = {
-    }
+    products = Product.objects.all().order_by('-date')
+    context = {'products': products}
     return render(request, 'index.html', context)
+
+def add_new_product(request):
+    if request.method == 'POST':
+        Product.objects.create(
+            title=request.POST.get('title'),
+            price=request.POST.get('price'),
+            description_product=request.POST.get('description_product'),
+            category_id=request.POST.get('category'),
+            image=request.POST.get('image')
+        )
+        return redirect('home_page')
+
+    categories = Category.objects.all()
+    return render(request, 'create_new_pos_product.html', {'categories': categories})
+
+def add_new_category(request):
+    if request.method == 'POST':
+        Category.objects.create(
+            title=request.POST.get('title'),
+            description=request.POST.get('description'),
+        )
+        return redirect('home_page')
